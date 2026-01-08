fa8087e24898d9b4e93152b29540562c802bf625
DevAryann <wannacodearyan@gmail.com>
2026-01-08 19:00:26 +0530

    Example for access modifier

---
+++ OOP/src/AccessModifiers/Example.java
@@ -0,0 +1,25 @@
+package AccessModifiers;
+
+public class Example {
+    public static void main(String[] args) {
+        Passwords acc1 = new Passwords();
+        acc1.setPasswords("abcd");
+
+        System.out.println(acc1.getPasswords());
+
+
+    }
+
+}
+
+class Passwords{
+   private String passwords ;
+
+   public void setPasswords(String pass ){                     // Setter
+       this.passwords = pass ;
+    }
+   public String getPasswords(){                               //getter
+       return this.passwords;
+
+   }
+}
