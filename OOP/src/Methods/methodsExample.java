45eb16a21cc147361e6341176f78b848ab125659
DevAryann <wannacodearyan@gmail.com>
2026-01-12 19:38:46 +0530

    Added Example for Methods

---
+++ OOP/src/Methods/methodsExample.java
@@ -0,0 +1,22 @@
+package Methods;
+
+public class methodsExample {
+
+    public static void main(String[] args) {
+
+        int a = 2 ;
+        int b = 3 ;
+
+        System.out.println(add(a,b));
+
+
+    }
+
+    static int add(int a , int b){
+
+        return  a +  b ;
+    }
+
+
+
+}
