000f8f0c015f64392b254fb67dd838594eb20928
DevAryann <wannacodearyan@gmail.com>
2026-01-19 17:56:34 +0530

    Example For WrapperClass

---
+++ OOP/src/WrapperClass/Main.java
@@ -0,0 +1,20 @@
+package WrapperClass;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        // AutoBoxing
+
+        Integer I = 123;
+        Double D = 3.14;
+        Boolean B = false;
+        String S = "aryan";
+        Character C = '&' ;
+
+        //Unboxing
+
+
+        System.out.println(C);
+    }
+}
