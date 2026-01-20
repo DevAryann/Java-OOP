e968cde6b07001744f729f2a7e90905b73626caa
DevAryann <wannacodearyan@gmail.com>
2026-01-20 19:20:42 +0530

    Example For Super

--- OOP/src/Super/Main.java
+++ OOP/src/Super/Main.java
@@ -3,7 +3,7 @@ package Super;
 public class Main {
     public static void main(String[] args) {
         Person p1 = new Person("Aryan","Jadhao");
-        Student s1 = new Student("Aryan","Jadhao",9.2);
+        Student s1 = new Student("Aryan","Jadhao","Dilip", 9.2);
 
         p1.info();
         s1.info();
--- OOP/src/Super/Student.java
+++ OOP/src/Super/Student.java
@@ -3,12 +3,14 @@ package Super;
 public class Student extends Person {
     String FirstName ;
     String LastName;
+    String middle ;
 
     double GPA ;
 
 
-    Student(String first , String last , double gpa){
+    Student(String first , String last, String middle , double gpa){
         super(first,last);
         this.GPA = gpa ;
+        this.middle = middle ;
     }
 }
